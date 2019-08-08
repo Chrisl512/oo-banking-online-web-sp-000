9e632faa16bac1323e2d66979e89d2dbf78b0960
Christopher Luna <chris@illusionlayervfx.com>
2019-08-08 03:28:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :status, :amount
+  attr_accessor :account, :balance
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @sender.balance
+    receiver.balance = balance + amount
+      binding.pry
+     = @balance - @amount
+
+
+    status = "complete"
+  end
+  end
+
 end
